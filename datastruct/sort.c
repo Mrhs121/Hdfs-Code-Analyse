328c9b9f2942f84cef4b052a8f3889830af689fe
root <1367467330@qq.com>
2018-10-10 18:56:58 +0800

    add sort

--- datastruct/sort.c
+++ datastruct/sort.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 
+void print(int b[],int n){
+    int i;
+    for(i=0;i<n;i++){
+        printf("%5d->",b[i]);
+    }
+    printf("\n");
+}
+
 void swap(int * a,int * b){
     int temp = *a;
     *a = *b;
@@ -29,23 +37,109 @@ void InsertSort(int arr[],int length){
     }
 }
 
-void print(int b[],int n){
-    int i;
-    for(i=0;i<n;i++){
-        printf("%5d",b[i]);
+
+void HalfInsertSort(int arr[],int n){
+    if(n<=1){
+        return;
     }
-    printf("\n");
+    int count=1;
+    int temp;
+    int i,j,mid,left,right;
+    for(i=1;i<n;i++){
+        if(arr[i]<arr[i-1]){
+            temp = arr[i];
+            left = 0;
+            right = i-1;
+            while(left<=right){
+                mid = (left + right)/2;
+                if(temp<arr[mid])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }
+            //arr[mid] = temp;
+            for(j=i-1;j>=mid;j--){
+                arr[j+1] = arr[j];
+            }
+            arr[mid] = temp;
+            printf("第%d次: ",count++);
+            print(arr,10);            
+        }
+    }
 }
+// 双向 冒泡
+void dobule_pop(int arr[],int n){
+    
+}
+
+void QuickSort(int arr[],int left,int right){
+    
+
+}
+
+// Time O(n^2) 
+// Space O(1)
+void selectSort(int arr[],int length){
+    if(length <= 1){
+        return;
+    }
+    int i,j,index;
+    int _min;
+    for(i=0;i<length-1;i++){
+        _min = arr[i];
+        index = i;
+        for(j=i;j<length;j++){
+            if(arr[j]<_min){
+                _min = arr[j];
+                index = j;
+            }
+                
+        }
+        //min = 9999;
+        swap(&arr[i],&arr[index]);
+    }
+}
+
+int find_k_min(int arr[],int low,int high,int k){
+    int pivot = arr[low];
+    int l=low,r=high;
+    while(low<high){
+        while(arr[high]>=pivot && low<high){
+            --high;
+        }
+        while(arr[low]<=pivot&&low<high){
+            ++low;
+        }
+        int temp = arr[high];
+        arr[high] = arr[low];
+        arr[low] = temp;
+
+    }
+    arr[l] = arr[low];
+    arr[low] = pivot;
+    if(low==k){
+        return arr[k-1];
+    }
+    else if(low>k){
+        return find_k_min(arr,l,low-1,k);
+    } else {
+        return find_k_min(arr,low+1,r,k);
+    }
+}
+
 int main()
 {
-    printf("Hello world\n");
-    int a[2] = {1,2};
-    swap(&a[0],&a[1]);
-    printf("%d\n",a[0]);
-    int b[10] = {12,2,4,51,213,14,5,5,1,1324};
+    int b[10] = {412,1234,124,6,1234,1,412,4231,4 ,3,};
+//    int k_min = find_k_min(b,0,9,3);
+    //printf("k_min = %d\n",k_min);
     print(b,10);
-    InsertSort(b,10);
+    selectSort(b,10);
     print(b,10);
+    //print(b,10);
+    //HalfInsertSort(b,10);
+    //print(b,10);
+
+    
     return 0;
 }
 
