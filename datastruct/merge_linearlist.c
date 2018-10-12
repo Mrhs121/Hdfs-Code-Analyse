a190f1cd1aee7617b682f3da299da80bd03931ee
root <1367467330@qq.com>
2018-10-12 17:10:31 +0800

    fix mergelist bug

--- datastruct/merge_linearlist.c
+++ datastruct/merge_linearlist.c
@@ -14,17 +14,17 @@ typedef struct{
 }Element;
 
 typedef struct{
-	Element data[MAXSIZE];
+	int data[MAXSIZE];
 	int length;
 }SqList;
 
 
-void merge(SqList A,SqList B,SqList &C)
+void merge(SqList A,SqList B,SqList C)
 {
 	int i=0,j=0,k=0;
 	while(i<A.length&&j<B.length)
 	{
-		if(A.data[i].data<=B.data[j].data)
+		if(A.data[i]<=B.data[j])
 			C.data[k++] = A.data[i++]; 
 		else 
 			C.data[k++] = B.data[j++];
@@ -42,25 +42,40 @@ void merge(SqList A,SqList B,SqList &C)
 
 }
 
-// 17年839真题  合并两个有序的数组，不使用缓冲区，A的长度够长
-void mergeWithoutBuffer(SqList A,SqList B,SqList &C)
-{
-	int i=0,j=0,k=0;
-	while(i<A.length&&j<B.length)
-	{
-		if(A.data[i].data<=B.data[j].data)
-			C.data[k++] = A.data[i++]; 
-		else 
-			C.data[k++] = B.data[j++];
-	}
-	while(i<A.length){
-		C.data[k++] = A.data[i++];	
-	}
-	while(j<B.length ){
-		C.data[k++] = B.data[j++];
-	}
 
-	C.length = k+1;
+void move(SqList * A,int current){
+    int len = A->length;
+    int i;
+    for(i=len-1;i>current;i--){
+        A->data[i] = A->data[i-1]; 
+    }
+}
+// 17年839真题  合并两个有序的数组，不使用缓冲区，A的长度够长
+// 在A中插入新的元素之后，后面的元素往后面移动即可，应为A 的长度够长
+void mergeWithoutBuffer(SqList *A,SqList *B)
+{
+    void print(int arr[],int n);
+	int i=0,j=0;
+	while(i<A->length&&j<B->length)
+	{
+		if(A->data[i]<=B->data[j])
+	        i++;
+        else{ 
+            A->length++;
+            move(A,i);
+            A->data[i] = B->data[j];
+            print(A->data,A->length);
+            i++;
+            j++;
+	    }
+    }
+    printf("i=%d,j=%d\n",i,j);
+	while(j<B->length ){
+	    printf("add B\n");
+        A->data[i++] = B->data[j++];   
+        A->length++;
+    }
+
 //	return ture;
 }
 
@@ -143,6 +158,7 @@ int findMid(int size,int a[],int b[]){
 	if(j==size){
 		return a[0];
 	}
+    return  -1;
 	// 复杂度O(n);
 	
 }
@@ -150,9 +166,30 @@ int main()
 {
 
 	int a[5] = {11,13,15,17,19};
-	int b[5] = {2,4,6,20,30};
-	int middata = findMid(5,a,b);
-	printf("the middata is :%d\n",middata);
+    SqList A,B;
+    A.length = 0;
+    B.length = 0;
+    int i;
+    for(i=0;i<5;i++){
+        A.data[i] = a[i];
+        A.length++;
+    }
+    //A.data[0] = 1000;
+    printf("test-->%d\n",A.data[0]);
+	int b[5] = {2,4,6,7,8};
+	for(i=0;i<5;i++){
+        B.data[i] = b[i];
+        B.length++;
+    }
+    print(a,5);
+    print(b,5);
+    mergeWithoutBuffer(&A,&B);
+    printf("\n meger result: A len = %d :",A.length);
+    for(i=0;i<A.length;i++){
+        printf("%3d",A.data[i]);
+    }
+    //int middata = findMid(5,a,b);
+	//printf("the middata is :%d\n",middata);
 	//int a[100] = {1,2,3,4,5,6,7,100,101,102};
 	//searchExchangeInsert(a,10,77);
 	//print(a,11);
@@ -186,6 +223,7 @@ int main()
 	}
 	printf("\n-------------------------------------------\n");
 */
+    printf("\n");
 	return 0;
 
 	}
